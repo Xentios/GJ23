16aa955c150c8c05ec044b6654fbac293a02a5c2
Xentios <xenq@hotmail.com>
2023-11-29 12:52:43 +0300

    Fixed Space Button Camera

--- Assets/Scripts/Singe Use Scripts/SpaceCameraPriority.cs
+++ Assets/Scripts/Singe Use Scripts/SpaceCameraPriority.cs
@@ -1,18 +1,31 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceCameraPriority : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    CinemachineBrain cinemachineBrain;
+
+    [SerializeField]
+    private CinemachineVirtualCamera myCinemachineVirtualCamera;
+
+    private ICinemachineCamera lastActiveCam;
+  
+
+    public void ActiveMyCamera()
     {
-        
+        lastActiveCam = cinemachineBrain.ActiveVirtualCamera;
+        if (lastActiveCam.Equals(myCinemachineVirtualCamera)) return;
+        myCinemachineVirtualCamera.Priority = 11;
+       
+        //myCinemachineVirtualCamera.SOlo
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DeActiveMyCamera()
     {
-        
+        if (lastActiveCam.Equals(myCinemachineVirtualCamera)) return;
+        myCinemachineVirtualCamera.Priority = 9;
     }
 }
